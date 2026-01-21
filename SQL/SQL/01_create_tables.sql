4f904445008a5ca1a54725688f7addeff02f6534
Amol Tathe <amoltathe8080@gmail.com>
2026-01-21 18:51:07 +0530

    Create sales table in SQL database

---
+++ SQL/SQL/01_create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sales (
+    order_id INT,
+    order_date DATE,
+    customer_id INT,
+    product_name VARCHAR(100),
+    region VARCHAR(50),
+    sales_amount DECIMAL(10,2),
+    cost DECIMAL(10,2)
+);
